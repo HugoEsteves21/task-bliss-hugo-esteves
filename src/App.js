84c291e9696f6ba7989fa3ece08dbd86dc4d27b5
Hugo Esteves <hugo.veigaa@gmail.com>
2023-01-11 15:48:30 +0000

    starting to build the structure of the list of questions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import ListScreen from "./pages/ListScreen";
+
 function App() {
   return (
     <div className="App">
-      <Routes></Routes>
+      <Routes>
+        <Route path='/' element={<ListScreen />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ListScreen.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import axios from "axios";
+
+function ListScreen() {
+  const [questions, setQuestions] = useState([]);
+  const [health, setHealth] = useState("");
+  const [limit, setLimit] = useState(10);
+  const [offset, setOffset] = useState(10);
+  const [filter, setFilter] = useState(null);
+
+  // create function that uses axios to retrieve information from the API, in these case, the qustions
+  const getQuestions = async () => {
+    try {
+      const response = await axios.get(
+        "https://private-anon-23d6aa8ecf-blissrecruitmentapi.apiary-mock.com/questions?limit=${limit}&offset=${offset}&filter=${filter}"
+      );
+
+      setQuestions(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // this hook will write the information on the page everytime it's refreshed
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  // First wi'll map over the list os questions stored on the "questions" array
+  // Then we'll map over the languages available to vote and the vote itself
+  return (
+    <div>
+      <h1>hello there!</h1>
+
+      {questions.map((question) => {
+        return (
+          <article key={question.id}>
+            <img src={question.image_url} alt="question" />
+            <NavLink to={`/questions/${question.id}`}>
+              <h2>{question.question}</h2>
+            </NavLink>
+            {/* <h2>{question.question}</h2> */}
+
+            <h4>Voting Options:</h4>
+            {question.choices.map((language) => {
+              return (
+                <article>
+                  <h6>
+                    <b>Language:</b> {language.choice}
+                  </h6>
+                  <h6>
+                    <b>Votes:</b> {language.votes}
+                  </h6>
+                </article>
+              );
+            })}
+          </article>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ListScreen;
